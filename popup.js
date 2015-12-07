302153767090ce5a23c5b333b4bbb641b8a236ef
srikarrohit <srikarrohit@gmail.com>
2015-12-07 22:32:39 +0530

    changes being saved

--- popup.js
+++ popup.js
@@ -1,23 +1,38 @@
 window.onload = function(){
 	chrome.storage.sync.get(function(items){
 					console.log(items.rollno);
-					document.getElementById("rollno").value=String(items.rollno);
-					document.getElementById("pass").value=String(items.pass);
-					alert(pass.value);
-					document.getElementById("form").submit();
+				document.getElementById("rollno").value=items.rollno;
+				document.getElementById("pass").value=items.pass;
+//					alert(pass.value);
+//					document.forms["form"].submit();
 					});
-	document.forms["form"].addEventListener("submit",function(){
+	document.getElementById("form").addEventListener("submit",function(){
 //	var btn=document.getElementById("btn");
 	var rollno = document.getElementById("rollno").value;
 	var pass = document.getElementById("pass").value;
 //	console.log(1);
 	chrome.storage.sync.set({'rollno': rollno,'pass': pass}, function() {
           document.getElementById("test").innerHTML='Settings saved';
+					alert(1);
 					});
+/*	var namespace="sync";
+	chrome.storage.onChanged.addListener(function(changes,namespace) {
+        for (key in changes) {
+          var storageChange = changes[key];
+          console.log('Storage key "%s" in namespace "%s" changed. ' +
+                      'Old value was "%s", new value is "%s".',
+                      key,
+                      namespace,
+                      storageChange.oldValue,
+                      storageChange.newValue);
+        }
+      });*/
 	chrome.storage.sync.get(function(items){
 					console.log(items.rollno);
-					alert(items.rollno)
-					alert(items.pass);
+//					alert(String(items.rollno))
+//					alert(items.pass);
+					document.getElementById("rollno").value=items.rollno;
+          document.getElementById("pass").value=items.pass;
 					});
 });
 };
